cb00b66e101975cc2cb10b6e702a23c60b207283
Issarapap7 <film4777@gmail.com>
2021-11-04 23:04:40 +0700

    ตะกร้ายังไม่เสร็จ

--- fontend/src/app/userMode/buy-products/buy-products.component.ts
+++ fontend/src/app/userMode/buy-products/buy-products.component.ts
@@ -2,8 +2,8 @@ import { ThrowStmt } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { cartsType } from 'src/app/cart.model';
 import { CartService } from 'src/app/services/cart.service';
-
-
+import { BookService } from 'src/app/services/book.service';
+import { FormControl, FormGroup, Validators} from '@angular/forms'
 @Component({
   selector: 'app-buy-products',
   templateUrl: './buy-products.component.html',
@@ -15,8 +15,10 @@ export class BuyProductsComponent implements OnInit {
   cart: cartsType = [];
   show: boolean = true;
   price: number =0;
+  
+  amount = new FormControl(0)
 
-  constructor(private cs: CartService) { }
+  constructor(private cs: CartService, private BookService:BookService) { }
 
   ngOnInit(): void {
   }
@@ -31,5 +33,37 @@ export class BuyProductsComponent implements OnInit {
     this.cs.getFromBuy(this.item);
     
   }
+
+  updateMinusStock(){
+    //console.log(this.item.name+"--> newQ = "+this.newQ);
+    
+    this.item.quantity = this.item.quantity - Number(this.amount.value);
+
+    try {
+      this.BookService.updateBook(this.item).subscribe(
+        data => {
+          //this.products = data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    window.location.reload();
+  }
+  
+  buy(){
+    console.log(this.item);
+    console.log("จำนวนสินค้า "+this.amount.value);
+
+    if(this.amount.value == 0){
+      alert('คุณต้องสั่งซื้อตั้งแต่ 1 ชิ้น ขึ้นไป')
+    }else{
+      this.updateMinusStock();
+    }
+  }
+
 
 }
